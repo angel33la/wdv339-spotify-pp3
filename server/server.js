d3767d77f629ccdcbbd10b45896216f7e21ecb80
angel33la <angel33la@outlook.com>
2026-03-07 02:22:42 -0500

    Add MongoDB connection setup and update server port to 5000

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
