a0d71c875a340566357ca1f1eccc5ed6f834660d
angel33la <angel33la@outlook.com>
2026-03-07 06:30:05 -0500

    Refactor Cloudinary integration: move configuration to a separate file and simplify image upload function

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -1,41 +1,19 @@
 import { v2 as cloudinary } from "cloudinary";
 
-// Configure Cloudinary with your credentials
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
+// helper function for cloudinary uploads
+const uploadToCloudinary = async (file) => {
+	try {
+		const result = await cloudinary.uploader.upload(file.tempFilePath, {
+			resource_type: "auto",
+		});
+		return result.secure_url;
+	} catch (error) {
+		console.log("Error in uploadToCloudinary", error);
+		throw new Error("Error uploading to cloudinary");
+	}
+};
 
-export const uploadImage = async (req, res) => {
-  try {
-    const file = req.file; // Assuming you're using multer to handle file uploads
-
-    if (!file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-
-    // Upload the image to Cloudinary
-    const result = await cloudinary.uploader.upload(file.path, {
-      folder: "your_folder_name", // Optional: specify a folder in Cloudinary
-    });
-      
-      const url =
-        process.env.CLOUDINARY_URL +
-        {
-          transformation: [
-            { fetch_format: "auto" },
-            { quality: "auto" },
-            { width: 200, height: 200, crop: "thumb", gravity: "face" },
-          ],
-        };
-      console.log("url:", url);
-
-    // Return the URL of the uploaded image
-    res.json({ imageUrl: result.secure_url });
-  } catch (error) {
-    console.error("Error uploading image:", error);
-    res.status(500).json({ error: "Failed to upload image" });
-  }
+export const checkAdmin = async (req, res, next) => {
+  res.status(200).json({ admin: true });
 };
