11c46cf9d22b116595911c639f88cf671301629c
angel33la <angel33la@outlook.com>
2026-03-13 21:32:22 -0400

    Refactor authentication controller and routes: update login function to comment out user validation and change authCallback to callback in routes

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,13 +1,13 @@
 import bcrypt from "bcryptjs";
 import { User } from "../models/user.model.js";
 
-import GOOGLE_AUTH_URL from "https://accounts.google.com/o/oauth2/v2/auth";
-import GOOGLE_TOKEN_URL from "https://oauth2.googleapis.com/token";
-import GOOGLE_USERINFO_URL from "https://www.googleapis.com/oauth2/v3/userinfo";
+const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+const GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
+const GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo";
 
 // login route - redirect to Google for authentication
 export const login = async (req, res) => {
-  try {
+ /* try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
@@ -15,20 +15,19 @@ export const login = async (req, res) => {
     // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid credentials" });*/
 
     // Proceed with JWT token generation for session
-    res.status(200).json({ message: "Login successful", userId: user._id });
-  } catch (error) {
+    res.status(200).json({ message: "Login successful", /*userId: user._id */ });
+ /* } catch (error) {
     res.status(500).json({ error: error.message });
-  }
+  }*/
 };
 
 // Google OAuth callback route
 export const callback = async (req, res) => {
   res.status(200).json({ message: "Auth callback route is active" });
 };
-
 
 // logout route - simply respond with success message (client will handle token deletion)
 export const logout = async (_req, res) => {
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { authCallback, login, logout } from "../controllers/auth.controller.js";
+import { callback, login, logout } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.get("/login", login);
-router.get("/callback", authCallback);
+router.get("/callback", callback);
 router.get("/logout", logout);
 
 export default router;
