41b3081b0e8f6f0538f99d1f64a4469ec4ae1649
angel33la <angel33la@outlook.com>
2026-03-15 19:27:17 -0400

    Enhance UI: Update styles for PlaylistCard, Login page, and global CSS for improved aesthetics and consistency

--- client/src/components/playlists/PlayListCard.js
+++ client/src/components/playlists/PlayListCard.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function PlaylistCard({ playlist }) {
   return (
-    <div>
+    <div style={{ fontSize: "2rem", color: "#e14e84" }}>
       <h3>{playlist.name}</h3>
       <p>{playlist.songs.length} songs</p>
       <Link to={`/playlists/${playlist._id}`}>Open</Link>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
         height: "100vh",
       }}
     >
-      <h1 style={{ fontSize: "4rem", color: "#1a143c" }}>Music Search App</h1>
+      <h1 style={{ fontSize: "4rem", color: "#fff" }}>Music Search App</h1>
       <LoginButton />
     </div>
   );
